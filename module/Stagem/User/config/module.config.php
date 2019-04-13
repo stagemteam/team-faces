ddea0dcc2ebb7dae6db46bee8a2ffbfcbbf24f6b
unknown <bitbok123@gmail.com>
2019-04-13 23:39:24 +0300

    'Login'

--- module/Stagem/User/config/module.config.php
+++ module/Stagem/User/config/module.config.php
@@ -1,19 +1,29 @@
 <?php
+
 namespace Stagem\User;
 
 //use Zend\Authentication\AuthenticationService;
-
 use Zend\ServiceManager\AbstractFactory\ReflectionBasedAbstractFactory;
 
 return [
-
     'assetic_configuration' => require_once 'assets.config.php',
-
-
     // mvc
     'view_manager' => [
         'template_map' => [
-            'layout::admin-login' => __DIR__ . '/../view/layout/admin/login.phtml'
+            'layout::admin-login' => __DIR__ . '/../view/layout/admin/login.phtml',
         ],
-    ]
+        'prefix_template_path_stack' => [
+            'user::' => __DIR__ . '/../view/user',
+        ],
+    ],
+    // middleware
+    'templates' => [
+        'map' => [
+            'layout::admin-login' => __DIR__ . '/../view/admin/layout/login.phtml',
+        ],
+        'paths' => [
+            'admin-user' => [__DIR__ . '/../view/admin/user'],
+            'widget' => [__DIR__ . '/../view/admin/widget'],
+        ],
+    ],
 ];
