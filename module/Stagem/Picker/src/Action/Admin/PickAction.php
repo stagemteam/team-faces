e3c126a0e2bdbdda558c7e68d18d2374475b00bd
Serhii Popov <popow.serhii@gmail.com>
2019-04-13 22:36:23 +0300

    Composer.json update

---
+++ module/Stagem/Picker/src/Action/Admin/PickAction.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * The MIT License (MIT)
+ * Copyright (c) 2018 Stagem Team
+ * This source file is subject to The MIT License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * https://opensource.org/licenses/MIT
+ *
+ * @category Stagem
+ * @package Stagem_Amazon
+ * @author Serhii Popov <popow.serhii@gmail.com>
+ * @license https://opensource.org/licenses/MIT The MIT License (MIT)
+ */
+
+namespace Stagem\Picker\Action\Admin;
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Interop\Http\Server\RequestHandlerInterface;
+#use Psr\Http\Server\RequestHandlerInterface;
+use Zend\Router\RouteMatch;
+use Zend\View\Model\ViewModel;
+use Stagem\ZfcAction\Page\AbstractAction;
+use Zend\View\View;
+
+/**
+ * @package Stagem_Picker
+ */
+class PickAction extends AbstractAction
+{
+
+    protected $bestsellerTable;
+
+    protected $bestsellerGrid;
+
+    public function __construct(/*BestsellerTable $bestsellerTable, BestsellerGrid $bestsellerGrid*/)
+    {
+        //$this->bestsellerTable = $bestsellerTable;
+        //$this->bestsellerGrid = $bestsellerGrid;
+    }
+
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        return $handler->handle($request->withAttribute(ViewModel::class, $this->action($request)));
+    }
+
+    public function action(ServerRequestInterface $request)
+    {
+        /*$route = $request->getAttribute(RouteMatch::class);
+        $select = $this->bestsellerTable->getLastMonthBestsellers();
+
+        $this->bestsellerGrid->setCounter($this->bestsellerTable->getLastMonthDistinctCounter());
+        $this->bestsellerGrid->init();
+        $dataGrid = $this->bestsellerGrid->getDataGrid();
+        $dataGrid->setUrl($this->url()->fromRoute($route->getMatchedRouteName(), $route->getParams()));
+        $dataGrid->setDataSource($select, $this->bestsellerTable->getAdapter());
+        $dataGrid->render();
+        $dataGridVm = $dataGrid->getResponse();
+
+        return $dataGridVm;*/
+
+        return new ViewModel();
+    }
+}
