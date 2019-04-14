3d96e4366cc7db0a062e2821ee4a0408963659bb
Andrew Belov <fearess7@gmail.com>
2019-04-14 04:40:49 +0300

    Added user service

---
+++ module/Stagem/User/src/Service/UserService.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * The MIT License (MIT)
+ * Copyright (c) 2018 Stagem Team
+ * This source file is subject to The MIT License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * https://opensource.org/licenses/MIT
+ *
+ * @category Stagem
+ * @package Stagem_Order
+ * @author Serhii Popov <popow.serhii@gmail.com>
+ * @license https://opensource.org/licenses/MIT The MIT License (MIT)
+ */
+
+namespace Stagem\Statistic\Service;
+
+use DateTime;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityRepository;
+use Popov\ZfcCore\Service\DomainServiceAbstract;
+use Stagem\Order\Model\Repository\StatisticRepository;
+use Stagem\Statistic\Model\Statistic;
+
+/**
+ * @method EntityManager getObjectManager()
+ */
+class UserService extends DomainServiceAbstract
+{
+    protected $entity = Statistic::class;
+
+
+    /**
+     * This method changes order status after parsing in order to know
+     * what order was parsed the last in unusual situations.
+     *
+     * @param $userId
+     */
+    public function userStatistic($user)
+    {
+        //we will get last 30 days
+        $endedAt = new DateTime('now');
+        $startedAt = new DateTime('-30 days');
+        $data = $this->getRepository()->getStatisticForUser($startedAt, $endedAt, $user);
+
+        return $data;
+    }
+
+    public function userDashboardData($data)
+    {
+        $statisticDashboard = [
+            'total' => 0,
+            'successful' => 0,
+            'failed' => 0,
+            'involvementRate' => 0.0,
+        ];
+        $statisticDashboard ['total'] = count($data);
+        foreach ($data as $item) {
+            $statisticDashboard ['successful'] += $item->getStatus();
+            $statisticDashboard ['failed'] += $item->getStatus();
+        }
+        $statisticDashboard['involvementRate'] =
+            $statisticDashboard ['successful'] / $statisticDashboard ['total'] * 100;
+
+        return $statisticDashboard;
+    }
+}
