abbb95338ae4424004ef060dd3b924fb84d1d14f
Viktor Dmytruk <bitbok@i.ua>
2019-04-14 11:55:40 +0300

    Edited popup resullt

--- module/Stagem/Picker/view/assets/js/picker.js
+++ module/Stagem/Picker/view/assets/js/picker.js
@@ -3,17 +3,29 @@ var form = $('#picker-form');
 $('.answer', form).on('click', function (e) {
   event.preventDefault();
   var elm = $(this);
+  var hr = $('.modal-content .progresses');
   var guessed = parseInt(form.find('#user').val());
   var picked = parseInt(elm.data('user'));
 
 
   if (guessed === picked) {
     elm.addClass('success');
+    hr.addClass('success');
+    $('.modal-content .progresses .success').show( 2000 );
     console.log(guessed, picked);
   } else {
     elm.addClass('fail');
+    hr.addClass('fail');
+    $('.modal-content .progresses .fail').show( 2000 );
     console.log(guessed, picked);
   }
+  $('.guess-buddy .buddy').text(elm.find('.user-name').text());
+  setTimeout(function () {
+    $('#popup').addClass('result');
+    $('.modal-content .progresses').removeClass("fail");
+    $('.modal-content .progresses .fail').remove();
+    $('.modal-content .progresses .success').css('display', 'block');
+  }, 2000);
 
   $.ajax({
     url: "/admin/picker/pick",
